feb746e67013768e855e25a6714c7544e845bf0c
Sven Nilsen <bvssvni@gmail.com>
2014-07-06 18:37:34 +0200

    Rendering a cube

--- src/cube.rs
+++ src/cube.rs
@@ -1,32 +1,27 @@
 
 /*
-        2  --------- 3
+        3  ---------  2
           /       / |
-         /       /  |
+         /  top  /  |
      7  -------- 6  | 1
        |        |  /
-       |        | /
+left   |  front | /  right
        |        |/
      4  -------- 5
 
-          
-       ---- ---- ---- ----
-      |    |    |    |    |
-      |    |    |    |    |
-       ---- ---- ---- ----
 */
 
-static cube_quads: [uint, ..24] = [
-    4u, 5, 6, 7,
-    5, 1, 3, 6,
-    1, 0, 2, 3,
-    0, 4, 7, 2,
-    7, 6, 3, 2,
-    0, 1, 5, 4,
+pub static QUADS: [uint, ..24] = [
+    0u, 4, 3, 7,    // left
+    5, 1, 6, 2,     // right
+    0, 1, 4, 5,     // bottom
+    3, 2, 7, 6,     // top
+    4, 5, 7, 6,     // front
+    1, 0, 2, 3,     // back
 ];
 
 // Cube vertices.
-static cube_vertices: [f32, ..24] = [
+pub static VERTICES: [f32, ..24] = [
     // This is the back surface
     -1.0f32,    -1.0,       1.0, // 0
      1.0,       -1.0,       1.0, // 1
@@ -39,4 +34,67 @@ static cube_vertices: [f32, ..24] = [
      1.0,        1.0,      -1.0, // 6
     -1.0,        1.0,      -1.0  // 7
 ];
-       
+
+
+#[repr(uint)]
+#[deriving(FromPrimitive)]
+pub enum Face {
+    Left = 0,
+    Right = 1,
+    Bottom = 2,
+    Top = 3,
+    Front = 4,
+    Back = 5,
+}
+
+impl Face {
+    #[inline(always)]
+    fn ind(self, vertex: uint, dim: uint) -> uint {
+        QUADS[self as uint * 4 + vertex] * 3 + dim
+    }
+
+    pub fn vertices(self, scale: f32) -> [f32, ..12] {
+        [
+            VERTICES[self.ind(0, 0)] * scale, 
+            VERTICES[self.ind(0, 1)] * scale, 
+            VERTICES[self.ind(0, 2)] * scale,
+    
+            VERTICES[self.ind(1, 0)] * scale, 
+            VERTICES[self.ind(1, 1)] * scale, 
+            VERTICES[self.ind(1, 2)] * scale,
+    
+            VERTICES[self.ind(2, 0)] * scale, 
+            VERTICES[self.ind(2, 1)] * scale, 
+            VERTICES[self.ind(2, 2)] * scale,
+    
+            VERTICES[self.ind(3, 0)] * scale, 
+            VERTICES[self.ind(3, 1)] * scale, 
+            VERTICES[self.ind(3, 2)] * scale,
+        ]
+    }
+}
+
+pub struct FaceIterator {
+    face: uint,
+}
+
+impl FaceIterator {
+    pub fn new() -> FaceIterator {
+        FaceIterator {
+            face: 0
+        }
+    }
+}
+
+impl Iterator<Face> for FaceIterator {
+    fn next(&mut self) -> Option<Face> {
+        match self.face {
+            x if x < 6 => {
+                self.face += 1;
+                FromPrimitive::from_uint(x)
+            },
+            _ => None
+        }
+    }
+}
+
