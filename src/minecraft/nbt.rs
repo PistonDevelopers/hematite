72f953f2fcc395b7ac3a17172c2531cf2650d12b
Sven Nilsen <bvssvni@gmail.com>
2015-09-09 14:20:33 +0200

    Removed prefix on `Nbt` and suffix on `List` variants
    Closes https://github.com/PistonDevelopers/hematite/issues/156

--- src/minecraft/nbt.rs
+++ src/minecraft/nbt.rs
@@ -11,8 +11,6 @@ use flate2::read::{ GzDecoder, ZlibDecoder };
 use serialize;
 use serialize::hex::ToHex;
 
-use self::Nbt::*;
-use self::List::*;
 use self::DecoderError::*;
 
 /// Represents a NBT value
@@ -26,9 +24,9 @@ pub enum Nbt {
     Double(f64),
     ByteArray(Vec<u8>),
     IntArray(Vec<i32>),
-    NbtString(String),
-    NbtList(List),
-    NbtCompound(Compound)
+    String(String),
+    List(List),
+    Compound(Compound)
 }
 
 impl fmt::Debug for Nbt {
@@ -42,9 +40,9 @@ impl fmt::Debug for Nbt {
             Nbt::Double(x) => write!(f, "{:.1}", x),
             Nbt::ByteArray(ref x) => write!(f, "b<{}>", x[..].to_hex()),
             Nbt::IntArray(ref x) => write!(f, "{:?}", *x),
-            Nbt::NbtString(ref x) => write!(f, "\"{}\"", *x),
-            Nbt::NbtList(ref x) => write!(f, "{:?}", *x),
-            Nbt::NbtCompound(ref x) => write!(f, "{:?}", *x)
+            Nbt::String(ref x) => write!(f, "\"{}\"", *x),
+            Nbt::List(ref x) => write!(f, "{:?}", *x),
+            Nbt::Compound(ref x) => write!(f, "{:?}", *x)
         }
     }
 }
@@ -60,9 +58,9 @@ impl fmt::Display for Nbt {
             Nbt::Double(x) => write!(f, "{:.1}", x),
             Nbt::ByteArray(ref x) => write!(f, "<{}>", x[..].to_hex()),
             Nbt::IntArray(ref x) => write!(f, "{:?}", *x),
-            Nbt::NbtString(ref x) => write!(f, "\"{}\"", *x),
-            Nbt::NbtList(ref x) => write!(f, "{:?}", *x),
-            Nbt::NbtCompound(ref x) => write!(f, "{:?}", *x)
+            Nbt::String(ref x) => write!(f, "\"{}\"", *x),
+            Nbt::List(ref x) => write!(f, "{:?}", *x),
+            Nbt::Compound(ref x) => write!(f, "{:?}", *x)
         }
     }
 }
@@ -70,17 +68,17 @@ impl fmt::Display for Nbt {
 /// An ordered list of NBT values.
 #[derive(Clone, PartialEq, Debug)]
 pub enum List {
-    ByteList(Vec<i8>),
-    ShortList(Vec<i16>),
-    IntList(Vec<i32>),
-    LongList(Vec<i64>),
-    FloatList(Vec<f32>),
-    DoubleList(Vec<f64>),
-    ByteArrayList(Vec<Vec<u8>>),
-    IntArrayList(Vec<Vec<i32>>),
-    StringList(Vec<String>),
-    ListList(Vec<List>),
-    CompoundList(Vec<Compound>)
+    Byte(Vec<i8>),
+    Short(Vec<i16>),
+    Int(Vec<i32>),
+    Long(Vec<i64>),
+    Float(Vec<f32>),
+    Double(Vec<f64>),
+    ByteArray(Vec<Vec<u8>>),
+    IntArray(Vec<Vec<i32>>),
+    String(Vec<String>),
+    List(Vec<List>),
+    Compound(Vec<Compound>)
 }
 
 /// An unordered list of named NBT values.
@@ -103,31 +101,31 @@ impl Nbt {
     }
 
     pub fn as_byte(&self) -> Option<i8> {
-        match *self { Byte(b) => Some(b), _ => None }
+        match *self { Nbt::Byte(b) => Some(b), _ => None }
     }
 
     pub fn into_compound(self) -> Result<Compound, Nbt> {
-        match self { NbtCompound(c) => Ok(c), x => Err(x) }
+        match self { Nbt::Compound(c) => Ok(c), x => Err(x) }
     }
 
     pub fn into_compound_list(self) -> Result<Vec<Compound>, Nbt> {
-        match self { NbtList(CompoundList(c)) => Ok(c), x => Err(x) }
+        match self { Nbt::List(List::Compound(c)) => Ok(c), x => Err(x) }
     }
 
     pub fn as_bytearray<'a>(&'a self) -> Option<&'a [u8]> {
-        match *self { ByteArray(ref b) => Some(&b[..]), _ => None }
+        match *self { Nbt::ByteArray(ref b) => Some(&b[..]), _ => None }
     }
 
     pub fn into_bytearray(self) -> Result<Vec<u8>, Nbt> {
-        match self { ByteArray(b) => Ok(b), x => Err(x) }
+        match self { Nbt::ByteArray(b) => Ok(b), x => Err(x) }
     }
 
     pub fn as_float_list<'a>(&'a self) -> Option<&'a [f32]> {
-        match *self { NbtList(FloatList(ref f)) => Some(&f[..]), _ => None }
+        match *self { Nbt::List(List::Float(ref f)) => Some(&f[..]), _ => None }
     }
 
     pub fn as_double_list<'a>(&'a self) -> Option<&'a [f64]> {
-        match *self { NbtList(DoubleList(ref d)) => Some(&d[..]), _ => None }
+        match *self { Nbt::List(List::Double(ref d)) => Some(&d[..]), _ => None }
     }
 }
 
@@ -136,7 +134,7 @@ impl<'a> Index<&'a str> for Nbt {
 
     fn index<'b>(&'b self, s: &'a str) -> &'b Nbt {
         match *self {
-            NbtCompound(ref c) => c.get(s).unwrap(),
+            Nbt::Compound(ref c) => c.get(s).unwrap(),
             _ => panic!("cannot index non-compound Nbt ({:?}) with '{}'", self, s)
         }
     }
@@ -244,19 +242,19 @@ impl<R: Read> NbtReader<R> {
         match try!(self.i8()) {
             TAG_END => {
                 assert_eq!(try!(self.i32()), 0);
-                Ok(CompoundList(Vec::new()))
+                Ok(List::Compound(Vec::new()))
             }
-            TAG_BYTE => self.array(|r| r.i8()).map(ByteList),
-            TAG_SHORT => self.array(|r| r.i16()).map(ShortList),
-            TAG_INT => self.array(|r| r.i32()).map(IntList),
-            TAG_LONG => self.array(|r| r.i64()).map(LongList),
-            TAG_FLOAT => self.array(|r| r.f32()).map(FloatList),
-            TAG_DOUBLE => self.array(|r| r.f64()).map(DoubleList),
-            TAG_BYTE_ARRAY => self.array(|r| r.array_u8()).map(ByteArrayList),
-            TAG_INT_ARRAY => self.array(|r| r.array(|r| r.i32())).map(IntArrayList),
-            TAG_STRING => self.array(|r| r.string()).map(StringList),
-            TAG_LIST => self.array(|r| r.list()).map(ListList),
-            TAG_COMPOUND => self.array(|r| r.compound()).map(CompoundList),
+            TAG_BYTE => self.array(|r| r.i8()).map(List::Byte),
+            TAG_SHORT => self.array(|r| r.i16()).map(List::Short),
+            TAG_INT => self.array(|r| r.i32()).map(List::Int),
+            TAG_LONG => self.array(|r| r.i64()).map(List::Long),
+            TAG_FLOAT => self.array(|r| r.f32()).map(List::Float),
+            TAG_DOUBLE => self.array(|r| r.f64()).map(List::Double),
+            TAG_BYTE_ARRAY => self.array(|r| r.array_u8()).map(List::ByteArray),
+            TAG_INT_ARRAY => self.array(|r| r.array(|r| r.i32())).map(List::IntArray),
+            TAG_STRING => self.array(|r| r.string()).map(List::String),
+            TAG_LIST => self.array(|r| r.list()).map(List::List),
+            TAG_COMPOUND => self.array(|r| r.compound()).map(List::Compound),
             tag_type => panic!("Unexpected tag type {}", tag_type)
         }
     }
@@ -267,17 +265,17 @@ impl<R: Read> NbtReader<R> {
             tag_type => {
                 let name = try!(self.string());
                 Some((try!(match tag_type {
-                    TAG_BYTE => self.i8().map(Byte),
-                    TAG_SHORT => self.i16().map(Short),
-                    TAG_INT => self.i32().map(Int),
-                    TAG_LONG => self.i64().map(Long),
-                    TAG_FLOAT => self.f32().map(Float),
-                    TAG_DOUBLE => self.f64().map(Double),
-                    TAG_BYTE_ARRAY => self.array_u8().map(ByteArray),
-                    TAG_INT_ARRAY => self.array(|r| r.i32()).map(IntArray),
-                    TAG_STRING => self.string().map(NbtString),
-                    TAG_LIST => self.list().map(NbtList),
-                    TAG_COMPOUND => self.compound().map(NbtCompound),
+                    TAG_BYTE => self.i8().map(Nbt::Byte),
+                    TAG_SHORT => self.i16().map(Nbt::Short),
+                    TAG_INT => self.i32().map(Nbt::Int),
+                    TAG_LONG => self.i64().map(Nbt::Long),
+                    TAG_FLOAT => self.f32().map(Nbt::Float),
+                    TAG_DOUBLE => self.f64().map(Nbt::Double),
+                    TAG_BYTE_ARRAY => self.array_u8().map(Nbt::ByteArray),
+                    TAG_INT_ARRAY => self.array(|r| r.i32()).map(Nbt::IntArray),
+                    TAG_STRING => self.string().map(Nbt::String),
+                    TAG_LIST => self.list().map(Nbt::List),
+                    TAG_COMPOUND => self.compound().map(Nbt::Compound),
                     tag_type => panic!("Unexpected tag type {}", tag_type)
                 }), name))
             }
@@ -329,7 +327,7 @@ impl Decoder {
 // }
 
 macro_rules! expect(
-    ($s:expr, $t:ident) => ({
+    ($s:expr, $t:path) => ({
         match $s.pop() {
             Ok($t(v)) => Ok(v),
             Ok(other) => {
@@ -348,22 +346,22 @@ impl serialize::Decoder for Decoder {
         Err(ExpectedError("()".to_string(), try!(self.pop()).to_string()))
     }
 
-    fn read_u64(&mut self) -> DecodeResult<u64> { expect!(self, Long as u64) }
-    fn read_u32(&mut self) -> DecodeResult<u32> { expect!(self, Int as u32) }
-    fn read_u16(&mut self) -> DecodeResult<u16> { expect!(self, Short as u16) }
-    fn read_u8 (&mut self) -> DecodeResult<u8>  { expect!(self, Byte as u8) }
+    fn read_u64(&mut self) -> DecodeResult<u64> { expect!(self, Nbt::Long).map(|x| x as u64) }
+    fn read_u32(&mut self) -> DecodeResult<u32> { expect!(self, Nbt::Int).map(|x| x as u32) }
+    fn read_u16(&mut self) -> DecodeResult<u16> { expect!(self, Nbt::Short).map(|x| x as u16) }
+    fn read_u8 (&mut self) -> DecodeResult<u8>  { expect!(self, Nbt::Byte).map(|x| x as u8) }
 
-    fn read_i64(&mut self) -> DecodeResult<i64> { expect!(self, Long) }
-    fn read_i32(&mut self) -> DecodeResult<i32> { expect!(self, Int) }
-    fn read_i16(&mut self) -> DecodeResult<i16> { expect!(self, Short) }
-    fn read_i8 (&mut self) -> DecodeResult<i8>  { expect!(self, Byte) }
+    fn read_i64(&mut self) -> DecodeResult<i64> { expect!(self, Nbt::Long) }
+    fn read_i32(&mut self) -> DecodeResult<i32> { expect!(self, Nbt::Int) }
+    fn read_i16(&mut self) -> DecodeResult<i16> { expect!(self, Nbt::Short) }
+    fn read_i8 (&mut self) -> DecodeResult<i8>  { expect!(self, Nbt::Byte) }
 
     fn read_isize(&mut self) -> DecodeResult<isize> {
         match try!(self.pop()) {
-            Byte(x) => Ok(x as isize),
-            Short(x) => Ok(x as isize),
-            Int(x) => Ok(x as isize),
-            Long(x) => Ok(x as isize),
+            Nbt::Byte(x) => Ok(x as isize),
+            Nbt::Short(x) => Ok(x as isize),
+            Nbt::Int(x) => Ok(x as isize),
+            Nbt::Long(x) => Ok(x as isize),
             other => Err(ExpectedError("isize".to_string(), other.to_string()))
         }
     }
@@ -375,8 +373,8 @@ impl serialize::Decoder for Decoder {
         Ok(try!(self.read_u8()) != 0)
     }
 
-    fn read_f64(&mut self) -> DecodeResult<f64> { expect!(self, Double) }
-    fn read_f32(&mut self) -> DecodeResult<f32> { expect!(self, Float) }
+    fn read_f64(&mut self) -> DecodeResult<f64> { expect!(self, Nbt::Double) }
+    fn read_f32(&mut self) -> DecodeResult<f32> { expect!(self, Nbt::Float) }
 
     fn read_char(&mut self) -> DecodeResult<char> {
         let s = try!(self.read_str());
@@ -392,7 +390,7 @@ impl serialize::Decoder for Decoder {
     }
 
     fn read_str(&mut self) -> DecodeResult<String> {
-        expect!(self, NbtString)
+        expect!(self, Nbt::String)
     }
 
     fn read_enum<T, F>(&mut self, _name: &str, f: F) -> DecodeResult<T>
@@ -405,10 +403,10 @@ impl serialize::Decoder for Decoder {
         where F: FnMut(&mut Self, usize) -> DecodeResult<T>
     {
         let name = match try!(self.pop()) {
-            NbtString(s) => s,
-            NbtCompound(mut o) => {
+            Nbt::String(s) => s,
+            Nbt::Compound(mut o) => {
                 let name = match o.remove("variant") {
-                    Some(NbtString(s)) => s,
+                    Some(Nbt::String(s)) => s,
                     Some(val) => return Err(ExpectedError("String".to_string(), val.to_string())),
                     None => return Err(MissingFieldError("variant".to_string()))
                 };
@@ -462,7 +460,7 @@ impl serialize::Decoder for Decoder {
     fn read_struct_field<T, F>(&mut self, name: &str, _idx: usize, f: F) -> DecodeResult<T>
         where F: FnOnce(&mut Self) -> DecodeResult<T>
     {
-        let mut obj = try!(expect!(self, NbtCompound));
+        let mut obj = try!(expect!(self, Nbt::Compound));
 
         let value = match obj.remove(name) {
             None => return Err(MissingFieldError(name.to_string())),
@@ -471,7 +469,7 @@ impl serialize::Decoder for Decoder {
                 try!(f(self))
             }
         };
-        self.push(NbtCompound(obj));
+        self.push(Nbt::Compound(obj));
         Ok(value)
     }
 
@@ -518,18 +516,18 @@ impl serialize::Decoder for Decoder {
     fn read_seq<T, F>(&mut self, f: F) -> DecodeResult<T>
         where F: FnOnce(&mut Self, usize) -> DecodeResult<T>
     {
-        let len = match try!(expect!(self, NbtList)) {
-            ByteList(list) => self.push_all(list, Byte),
-            ShortList(list) => self.push_all(list, Short),
-            IntList(list) => self.push_all(list, Int),
-            LongList(list) => self.push_all(list, Long),
-            FloatList(list) => self.push_all(list, Float),
-            DoubleList(list) => self.push_all(list, Double),
-            ByteArrayList(list) => self.push_all(list, ByteArray),
-            IntArrayList(list) => self.push_all(list, IntArray),
-            StringList(list) => self.push_all(list, NbtString),
-            ListList(list) => self.push_all(list, NbtList),
-            CompoundList(list) => self.push_all(list, NbtCompound)
+        let len = match try!(expect!(self, Nbt::List)) {
+            List::Byte(list) => self.push_all(list, Nbt::Byte),
+            List::Short(list) => self.push_all(list, Nbt::Short),
+            List::Int(list) => self.push_all(list, Nbt::Int),
+            List::Long(list) => self.push_all(list, Nbt::Long),
+            List::Float(list) => self.push_all(list, Nbt::Float),
+            List::Double(list) => self.push_all(list, Nbt::Double),
+            List::ByteArray(list) => self.push_all(list, Nbt::ByteArray),
+            List::IntArray(list) => self.push_all(list, Nbt::IntArray),
+            List::String(list) => self.push_all(list, Nbt::String),
+            List::List(list) => self.push_all(list, Nbt::List),
+            List::Compound(list) => self.push_all(list, Nbt::Compound)
         };
         f(self, len)
     }
@@ -543,11 +541,11 @@ impl serialize::Decoder for Decoder {
     fn read_map<T, F>(&mut self, f: F) -> DecodeResult<T>
         where F: FnOnce(&mut Self, usize) -> DecodeResult<T>
     {
-        let obj = try!(expect!(self, NbtCompound));
+        let obj = try!(expect!(self, Nbt::Compound));
         let len = obj.len();
         for (key, value) in obj.into_iter() {
             self.push(value);
-            self.push(NbtString(key));
+            self.push(Nbt::String(key));
         }
         f(self, len)
     }
